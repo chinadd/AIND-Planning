7782a0d472e0e18fe9088ad0ebc176f0021666fe
chinaddw <Xing.Du@umassmed.edu>
2017-05-14 18:10:04 +0800

    part1 revised

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -131,11 +131,17 @@ class AirCargoProblem(Problem):
         possible_actions = []
         kb = PropKB()
         kb.tell(decode_state(state, self.state_map).pos_sentence())
-        for action in self.get_actions():
+        for action in self.actions_list:
             #check each actions' pre-conditino against all kb.clauses
-            if action.check_precond(kb, action.args):
+            is_possible = True
+            for clause in action.precond_pos:
+                if clause not in kb.clauses:
+                    is_possible = False
+            for clause in action.precond_neg:
+                if clause in kb.clauses:
+                    is_possible = False
+            if is_possible:
                 possible_actions.append(action)
-
         return possible_actions
 
     def result(self, state: str, action: Action):
@@ -150,21 +156,19 @@ class AirCargoProblem(Problem):
         # TODO implement
         new_state = FluentState([], [])
         old_state = decode_state(state, self.state_map)
-        if action not in self.get_actions():
-            new_state = old_state
-        else:          
-            for fluent in old_state.pos:
-                if fluent not in action.effect_rem:
-                    new_state.pos.append(fluent)
-            for fluent in action.effect_add:
-                if fluent not in new_state.pos:
-                    new_state.pos.append(fluent)
-            for fluent in old_state.neg:
-                if fluent not in action.effect_add:
-                    new_state.neg.append(fluent)
-            for fluent in action.effect_rem:
-                if fluent not in new_state.neg:
-                    new_state.neg.append(fluent)
+                 
+        for fluent in old_state.pos:
+            if fluent not in action.effect_rem:
+                new_state.pos.append(fluent)
+        for fluent in action.effect_add:
+            if fluent not in new_state.pos:
+                new_state.pos.append(fluent)
+        for fluent in old_state.neg:
+            if fluent not in action.effect_add:
+                new_state.neg.append(fluent)
+        for fluent in action.effect_rem:
+            if fluent not in new_state.neg:
+                new_state.neg.append(fluent)
                 
         return encode_state(new_state, self.state_map)
 
